61a6e97cb2dd655cd0e3b4f5397c0cb69d6d1278
AkshatTukn <56080995+AkshatTukn@users.noreply.github.com>
2021-09-04 16:43:19 +0530

    Add files via upload
    Z algorithm for string matching

---
+++ Z.cpp
@@ -0,0 +1,21 @@
+vector<int>Zalgo(const string&s)
+{
+	vector<int>ret(s.size());
+	if(s.empty())return ret;
+	ret[0]=s.size();
+	int i=1,j=0;
+	while(i<s.size())
+	{
+		while(i+j<s.size()&&s[j]==s[i+j])j++;
+		ret[i]=j;
+		if(j==0)
+		{
+			i++;
+			continue;
+		}
+		int k=1;
+		while(i+k<s.size()&&k+ret[k]<j)ret[i+k]=ret[k],k++;
+		i+=k;j-=k;
+	}
+	return ret;
+}
