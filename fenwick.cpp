6fc67cbeca4b7611763f27934e576e84a768df22
Akshat Tuknait <56080995+AkshatTukn@users.noreply.github.com>
2021-09-29 14:58:25 +0530

    Add files via upload

---
+++ fenwick.cpp
@@ -0,0 +1,22 @@
+template <typename T>
+struct Fenwick {
+    const int n;
+    std::vector<T> a;
+    Fenwick(int n) : n(n), a(n) {}
+    void add(int x, T v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] += v;
+        }
+    }
+    T sum(int x) {
+        T ans = 0;
+        for (int i = x; i > 0; i -= i & -i) {
+            ans += a[i - 1];
+        }
+        return ans;
+    }
+    T rangeSum(int l, int r) {
+        return sum(r) - sum(l);
+    }
+};
+ 
