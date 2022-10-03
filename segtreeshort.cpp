98518ca52bd1721a6b1edb7ab97ce84f8488fa99
Akshat Tuknait <56080995+AkshatTukn@users.noreply.github.com>
2022-10-04 00:29:59 +0530

    Update segtreeshort.cpp

--- segtreeshort.cpp
+++ segtreeshort.cpp
@@ -1,48 +1,60 @@
-struct segtree{
-    vector<ll> st; //segement tree array (contains all nodes)
-    ll n;
-    void init(ll val){
-        n = val;
-        st.resize(n*4,0);
-    }
+struct segtree
+{ 
+  vector<PLL>store;
+  ll n;
+  void init(ll x)
+  {
+    n=x;
+    store.resize(x*4,{0,-1});
+  }
+void build(ll start,ll end,ll ind,vector<ll>vals)
+{
+      if(start == end)
+      {
+         store[ind] = {vals[start],start};
+         return;
+      }
  
-    void build(ll start,ll end,ll idx,vector<ll>&arr){
-        if(start==end){
-            st[idx] = arr[start]; return;
-        }
-        ll mid = start + (end-start)/2;
-        build(start,mid,2*idx + 1,arr);
-        build(mid+1,end,2*idx + 2,arr);
-        st[idx] = max(st[2*idx + 1],st[2*idx + 2]);
-    }
- 
-    ll query(ll start,ll end,ll idx,vector<ll>&arr,ll l,ll r){
-        if(l<start && r<start || l>end && r>end) return 0;
-        if(start>=l && end<=r) return st[idx];
-        ll mid = start + (end-start)/2;
-        ll q1 = query(start,mid,2*idx + 1,arr,l,r);
-        ll q2 = query(mid+1,end,2*idx + 2,arr,l,r);
-        return max(q1,q2);
-    }
- 
-    void update(ll start,ll end,ll idx,vector<ll>&arr,ll req_idx,ll req_val){
-        if(start==end){
-            st[idx] = req_val; return;
-        }
-        ll mid = start + (end-start)/2;
-        if(req_idx<=mid){
-            update(start,mid,2*idx + 1,arr,req_idx,req_val);
-        }
-        else{
-            update(mid+1,end,2*idx + 2,arr,req_idx,req_val);
-        }
-        st[idx] = max(st[2*idx + 1],st[2*idx + 2]);
-    }
- 
-    void display(){
-        for(auto itr:st){
-            cout<<itr<<" ";
-        }
-        cout<<en;
-    }
+ ll mid = start+ (end-start)/2;
+build(start,mid,2*ind+1,vals);
+build(mid+1,end,2*ind+2,vals);
+
+store[ind]=Max(store[2*ind+1],store[2*ind+2]);
+}
+
+pair<long,long>query1(ll start,ll end,ll ind,ll l, ll r)
+{
+    if((l < start && r<start ) || (l>end && r>end))
+          {
+             return {-9223372036854775807LL,0};
+         }
+ if(start>=l && end<=r)
+ {
+  return store[ind];
+ }
+ll mid= start + (end-start)/2;
+PLL OPS1= query1(start,mid,2*ind+1,l,r);
+PLL OPS2= query1(mid+1,end,2*ind+2,l,r);
+
+return Max(OPS1,OPS2);
+}
+void update(ll start,ll end,ll ind,ll need_ind,ll valAdd)
+{
+ if(start == end)
+ {
+     store[ind].F += valAdd;
+     return;
+ }
+
+ll mid = start+ (end-start)/2;
+if(need_ind<=mid)
+{
+ update(start,mid,2*ind+1,need_ind,valAdd);
+}else
+{
+  update(mid+1,end,2*ind+2,need_ind,valAdd);
+}
+store[ind]=Max(store[2*ind+1],store[2*ind+2]);
+}
+
 };
